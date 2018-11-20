d152d1ae7f033e1fcca1639fceb1c5386479983d
Dominik Kozlowski <kozlowskidominik6@gmail.com>
2018-11-20 09:17:41 +0100

    test githuba sout

--- githublab3/src/githublab3/Githublab3.java
+++ githublab3/src/githublab3/Githublab3.java
@@ -3,7 +3,7 @@ package githublab3;
 public class Githublab3 {
 
     public static void main(String[] args) {
-      
+        System.out.println("Test githuba");
     }
     elo motyka zara wyjaśni wam wszystko
 }
